23c0ffab786b5981b0c4ba24ee4f5a04f9f59166
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-07-01 17:19:32 +0100

    Initial Commit

---
+++ Glass Login Screen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Glass Login Screen
+//
+//  Created by Richard Nkanga on 01/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glass Login Screen/Glass_Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glass_Login_ScreenApp.swift
+//  Glass Login Screen
+//
+//  Created by Richard Nkanga on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Glass_Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
